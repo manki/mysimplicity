4be9274362a850dc2a4275cb1b18813f0f20d652
Muthu Kannan <m@manki.in>
2013-11-10 23:10:15 +1100

    Update code to use Pebble SDK 2.0.

--- src/mysimplicity.c
+++ src/mysimplicity.c
@@ -1,46 +1,26 @@
 #include <ctype.h>
 
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include "pebble.h"
 
 
-#define MY_UUID { 0xB8, 0x64, 0xF3, 0x8A, 0xF4, 0xEA, 0x47, 0x09, 0xB3, 0xE3, 0xF1, 0x30, 0x89, 0xEA, 0x0A, 0xF5 }
-PBL_APP_INFO(MY_UUID,
-             "mSimplicity", "Manki",
-             1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_WATCH_FACE);
+static Window *window;
 
-Window window;
+static TextLayer *text_date_layer;
+static TextLayer *text_time_layer;
+static TextLayer *text_other_time_layer;
 
-TextLayer text_date_layer;
-TextLayer text_time_layer;
-TextLayer text_other_time_layer;
-
-Layer line_layer;
+static Layer* window_layer;
+static Layer* line_layer;
 
 
 void line_layer_update_callback(Layer *me, GContext* ctx) {
-  (void)me;
-
-  graphics_context_set_stroke_color(ctx, GColorWhite);
-
-  graphics_draw_line(ctx, GPoint(8, 97), GPoint(131, 97));
-  graphics_draw_line(ctx, GPoint(8, 98), GPoint(131, 98));
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, layer_get_bounds(me), 0, GCornerNone);
 }
 
 
-void init_text_layer(TextLayer* text_layer, uint32_t font_resource_id) {
-  text_layer_init(text_layer, window.layer.frame);
-  text_layer_set_text_color(text_layer, GColorWhite);
-  text_layer_set_background_color(text_layer, GColorClear);
-  text_layer_set_font(text_layer, fonts_load_custom_font(resource_get_handle(font_resource_id)));
-}
-
-
-PblTm add_time(const PblTm* time, unsigned int hours, unsigned int minutes) {
-  PblTm result = *time;
+struct tm add_time(const struct tm* time, unsigned int hours, unsigned int minutes) {
+  struct tm result = *time;
 
   int result_minutes = minutes + time->tm_min;
   if (result_minutes >= 60) {
@@ -59,8 +39,8 @@ PblTm add_time(const PblTm* time, unsigned int hours, unsigned int minutes) {
 }
 
 
-PblTm subtract_time(const PblTm* time, unsigned int hours, unsigned int minutes) {
-  PblTm result = *time;
+struct tm subtract_time(const struct tm* time, unsigned int hours, unsigned int minutes) {
+  struct tm result = *time;
 
   int result_minutes = time->tm_min - minutes;
   if (result_minutes < 0) {
@@ -88,13 +68,13 @@ void maybe_remove_leading_zero(char* time_text, size_t bufsize) {
 }
 
 
-void show_time(const PblTm* time) {
+void show_time(const struct tm* time) {
   // Need to be static because they're used by the system later.
   static char date_text[] = "Xxx, Xxx 00";
   static char last_date_text[] = "Xxx, Xxx 00";
-  string_format_time(date_text, sizeof(date_text), "%a, %b %e", time);
+  strftime(date_text, sizeof(date_text), "%a, %b %e", time);
   if (strncmp(last_date_text, date_text, sizeof(date_text))) {
-    text_layer_set_text(&text_date_layer, date_text);
+    text_layer_set_text(text_date_layer, date_text);
     strncpy(last_date_text, date_text, sizeof(date_text));
   }
 
@@ -106,65 +86,86 @@ void show_time(const PblTm* time) {
   }
 
   static char time_text[] = "00:00";
-  string_format_time(time_text, sizeof(time_text), time_format, time);
+  strftime(time_text, sizeof(time_text), time_format, time);
   maybe_remove_leading_zero(time_text, sizeof(time_text));
-  text_layer_set_text(&text_time_layer, time_text);
+  text_layer_set_text(text_time_layer, time_text);
 
   static char other_time_text[] = "XXX  XX:XX XX";
-  PblTm other_time = add_time(time, 5, 30);
-  string_format_time(
-      other_time_text, sizeof(other_time_text), "SYD  %I:%M %p", &other_time);
+  struct tm other_time = subtract_time(time, 5, 30);
+  strftime(
+      other_time_text, sizeof(other_time_text), "IND  %I:%M %p", &other_time);
   maybe_remove_leading_zero(&(other_time_text[5]), sizeof(other_time_text));
-  text_layer_set_text(&text_other_time_layer, other_time_text);
+  text_layer_set_text(text_other_time_layer, other_time_text);
 }
 
 
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
+void init_text_layer(TextLayer* text_layer, uint32_t font_resource_id) {
+  text_layer_set_text_color(text_layer, GColorWhite);
+  text_layer_set_background_color(text_layer, GColorClear);
 
-  window_init(&window, "mSimplicity");
-  window_stack_push(&window, true /* Animated */);
-  window_set_background_color(&window, GColorBlack);
+  GFont font = fonts_load_custom_font(
+      resource_get_handle(font_resource_id));
+  text_layer_set_font(text_layer, font);
 
-  resource_init_current_app(&APP_RESOURCES);
-
-  init_text_layer(&text_date_layer, RESOURCE_ID_FONT_MONDA_21);
-  layer_set_frame(&text_date_layer.layer, GRect(8, 68, 144-8, 168-68));
-  layer_add_child(&window.layer, &text_date_layer.layer);
-
-  init_text_layer(&text_time_layer, RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49);
-  layer_set_frame(&text_time_layer.layer, GRect(7, 92, 144-7, 168-92));
-  layer_add_child(&window.layer, &text_time_layer.layer);
-
-  init_text_layer(&text_other_time_layer, RESOURCE_ID_FONT_MONDA_BOLD_SUBSET_16);
-  layer_set_frame(&text_other_time_layer.layer, GRect(14, 10, 144-14, 25));
-  layer_add_child(&window.layer, &text_other_time_layer.layer);
-
-  layer_init(&line_layer, window.layer.frame);
-  line_layer.update_proc = &line_layer_update_callback;
-  layer_add_child(&window.layer, &line_layer);
-
-  PblTm now;
-  get_time(&now);
-  show_time(&now);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
 
-void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
-  (void)ctx;
-  show_time(t->tick_time);
+void render_ui() {
+  text_date_layer = text_layer_create(GRect(8, 68, 144-8, 168-68));
+  init_text_layer(text_date_layer, RESOURCE_ID_FONT_MONDA_21);
+
+  text_time_layer = text_layer_create(GRect(7, 92, 144-7, 168-92));
+  init_text_layer(text_time_layer, RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49);
+
+  text_other_time_layer = text_layer_create(GRect(14, 10, 144-14, 25));
+  init_text_layer(text_other_time_layer,
+      RESOURCE_ID_FONT_MONDA_BOLD_SUBSET_16);
+
+  line_layer = layer_create(GRect(8, 97, 139, 2));
+  layer_set_update_proc(line_layer, line_layer_update_callback);
+  layer_add_child(window_layer, line_layer);
 }
 
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
+void init_window() {
+  window = window_create();
+  window_layer = window_get_root_layer(window);
 
-    .tick_info = {
-      .tick_handler = &handle_minute_tick,
-      .tick_units = MINUTE_UNIT
-    }
+  const bool animated = true;
+  window_stack_push(window, animated);
+  window_set_background_color(window, GColorBlack);
 
-  };
-  app_event_loop(params, &handlers);
+  render_ui();
+}
+
+
+void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
+  show_time(tick_time);
+}
+
+
+void init() {
+  init_window();
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+
+  time_t t = time(NULL);
+  struct tm* now = localtime(&t);
+  show_time(now);
+}
+
+
+void deinit() {
+  tick_timer_service_unsubscribe();
+  text_layer_destroy(text_date_layer);
+  text_layer_destroy(text_time_layer);
+  text_layer_destroy(text_other_time_layer);
+  window_destroy(window);
+}
+
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
 }
